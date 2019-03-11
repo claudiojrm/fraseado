c318d4c52e66caa1c6a0f4a47551f7f8eaf9c47d
Cláudio Jr. Melo <claudiojrm@gmail.com>
2019-03-11 01:02:41 -0300

    favoritar

--- components/post/view.jsx
+++ components/post/view.jsx
@@ -6,6 +6,49 @@ import Share from '../share/view';
  * @description Classe de Inicialização da view do componente Post
  */
 export default class Post extends Component {
+    state = {
+        ids : []
+    }
+
+    /**
+     * @memberof Post
+     * @method favorite
+     * @description Método para favoritar um post
+     */
+    favorite = e => {
+        e.preventDefault();
+
+        // id favoritado
+        const id = this.props.post.id;
+
+        // flag que indica se está favoritado
+        const favorite = this.state.ids.includes(id);
+
+        // ids de itens favoritados
+        let ids = JSON.parse(localStorage.getItem('favoritos')) || [];
+        ids = favorite ? ids.filter(i => i != id) : [...ids, id];
+
+        // adiciona o id no storage
+        localStorage.setItem('favoritos', JSON.stringify(ids));
+
+        // atualiza o state com os ids favoritados
+        this.setState({
+            ids
+        });
+    }
+
+    /**
+     * @memberof Post
+     * @method componentDidMount
+     * @description Método para atualizar os ids favoritados
+     */
+    componentDidMount() {
+        // atualiza o state com os ids favoritados
+        this.setState({
+            ids : JSON.parse(localStorage.getItem('favoritos')) || []
+        });
+    }
+
     /**
      * @memberof Post
      * @method render
@@ -13,10 +56,11 @@ export default class Post extends Component {
      */
     render() {
         const {post} = this.props;
+        const isFavorite = this.state.ids.includes(post.id);
 
         return(
             <article className="post">
-                <Share id={post.id} noshadow={!post.category && post.thumbnail} />
+                <Share isFavorite={isFavorite} favorite={this.favorite} noshadow={!post.category && post.thumbnail} />
 
                 {
                     post.category ? (<div className="category-info">
@@ -32,7 +76,7 @@ export default class Post extends Component {
                 <div className="post-content">
                     {
                         post.thumbnail ? (
-                            <figure><img src={'/public/uploads/' + post.thumbnail} width="100%" alt={post.excerpt} /></figure>
+                            <figure onDoubleClick={this.favorite}><img src={'/public/uploads/' + post.thumbnail} width="100%" alt={post.excerpt} /></figure>
                         ) : null
                     }
 
