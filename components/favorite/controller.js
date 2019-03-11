03b1af6647027eafc0bd296fa7cd55a37ccb70f5
Cláudio Jr. Melo <claudiojrm@gmail.com>
2019-03-11 19:46:33 -0300

    pagina de favoritos

--- components/favorite/controller.js
+++ components/favorite/controller.js
@@ -33,10 +33,13 @@ export default class Favorite {
             this.data.ids = JSON.parse(tools.request.query.ids);
         } catch(e) {}
 
+        // ids dos posts
+        const {ids} = this.data;
+
         // busca os dados do post
-        if(this.data.ids.length) {
+        if(ids.length) {
             const {records:posts} = await Neo4j.run('MATCH (p:Post)--(c:Category) WHERE p.id IN $props.ids WITH p, min(c.id) as id MATCH (p)--(c:Category)--(s:Category) WHERE c.id = id OPTIONAL MATCH (p)-[:ATTACHMENT]-(a:Attachment) RETURN DISTINCT s.slug + "/" + c.slug as slug,c.name, p.id, p.content, p.slug, a.file', {
-                ids : this.data.ids
+                ids
             });
 
             for(const post of posts) {
@@ -54,6 +57,9 @@ export default class Favorite {
 
                 this.data.posts.push(this.data.post);
             }
+
+            // reordena com base no id
+            this.data.posts = this.data.posts.sort((a, b) => ids.findIndex(id => id == a.id) < ids.findIndex(id => id == b.id));
         }
 
         return next(this.data);
