e56bfe045bb08799d2010a1db2311948fda73233
Cláudio Jr. Melo <claudiojrm@gmail.com>
2019-03-17 01:15:24 -0300

    menu

--- components/menu/controller.js
+++ components/menu/controller.js
@@ -28,12 +28,12 @@ export default class Menu {
         const Neo4j = new tools.Neo4j();
 
         // busca os dados da categoria
-        const {records:menus} = await Neo4j.run('MATCH (c:Category)-[:HOME]-(h:Home) RETURN c.name, c.slug UNION MATCH (:Category)<--(c:Category)--(p:Post) WITH COUNT(p) AS total, c WHERE total > 10 AND NOT c.name CONTAINS "2017" AND NOT c.name CONTAINS "2018" RETURN c.name, c.slug ORDER BY total DESC');
+        const {records:menus} = await Neo4j.run('MATCH (:Home)-[h:HOME]-(c:Category)-->(s:Category) RETURN c.name, s.slug + "/" + c.slug AS slug UNION MATCH (s:Category)<--(c:Category)--(p:Post) WITH COUNT(p) AS total, c, s WHERE total > 10 AND NOT c.name CONTAINS "2017" AND NOT c.name CONTAINS "2018" AND NOT s.slug CONTAINS "datas" RETURN c.name, s.slug + "/" + c.slug AS slug ORDER BY total DESC');
 
         for(const menu of menus) {
             this.data.submenu.push({
-                name : menu.get('c.name'),
-                href : config.base + menu.get('c.slug')
+                name : menu.get('c.name').replace(/Frases (d[aeo]\s|em\s)?/g, ''),
+                href : config.base + menu.get('slug')
             });
         }
 
